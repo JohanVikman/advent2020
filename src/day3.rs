b5bc571211e856637eb95808a1228a713a3454dc
Johan Vikman Lundström (jvikman) <jvikman@cisco.com>
2020-12-29 10:37:22 +0100

    day3: moved map into a struct (object)

--- src/day3.rs
+++ src/day3.rs
@@ -1,13 +1,56 @@
 use std::fs::File;
 use std::io::Read;
-use std::str::Chars;
+//use std::str::Chars;
 //use regex::Regex;
-//use std::convert::TryInto;
+use std::convert::TryInto;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 enum MapSymbol {
     Tree,
     Empty,
+}
+
+pub struct Map {
+    map: Vec<Vec<MapSymbol>>,
+    linelength: u32,
+}
+
+impl Map {
+    pub fn new(contents: &str) -> Map{
+        let mut map: Vec<Vec<MapSymbol>> = Vec::new();
+        let mut linelength = 0;
+        for line in contents.lines() {
+            if linelength == 0 {
+                linelength = line.len().try_into().unwrap();
+            }
+            map.push(line.chars().map( |c|
+                if c == '#' {MapSymbol::Tree}
+                else {MapSymbol::Empty}).collect::<Vec<MapSymbol>>());
+            }
+        println!("Pushed {} onto map", map.len());
+        Map{
+            map: map,
+            linelength: linelength,
+        }
+    }
+
+    pub fn count_trees(&mut self, xstep: u32, ystep: u32) -> u32 {
+        let mut x = 0;
+        let mut y = 0;
+        let mut ntrees = 0;
+        for row in &self.map {
+            if row[(x % self.linelength) as usize] == MapSymbol::Tree
+                {
+                    ntrees += 1;
+                }
+            else {
+                "Open".to_string();
+                }
+            x += xstep;
+            y += ystep;
+        }
+        return ntrees;
+    }
 }
 
 pub fn run() {
@@ -15,37 +58,12 @@ pub fn run() {
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Something went wrong ");
 
-    let mut ntrees = 0;
-    let mut map: Vec<Vec<MapSymbol>> = Vec::new();
-    let mut linelength = 0;
+    let mut map = Map::new(&contents);
 
-    for line in contents.lines() {
-        if linelength == 0 {
-            linelength = line.len();
-        }
-        map.push(line.chars().map( |c|
-            if c == '#' {MapSymbol::Tree}
-            else {MapSymbol::Empty}).collect::<Vec<MapSymbol>>());
-    }
-    println!("Pushed {} onto map", map.len());
-    let ystep = 1;
-    let xstep = 3;
-    let mut x = 0;
-    let mut y = 0;
-    for row in &map {
-        if row[x % linelength] == MapSymbol::Tree 
-            { 
-                println!("Found a tree on {} {}", y, x);
-                ntrees += 1;
-            } 
-        else {
-                "Open".to_string();
-            }
-        x += xstep;
-        y += ystep;
-    }
     //println!("{}", map);
     // work with the board (of vectors)
-    println!("Part1: N trees are {}", ntrees);
-    println!("Part2: Valid passwords {}", ntrees);
+    println!("Part1: N trees are {}", map.count_trees(3, 1));
+
+//Part 2:
+    println!("Part2: Valid passwords {}", 0);
 }
